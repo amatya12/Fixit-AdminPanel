6e2d0a50fdb6ff4f3f9ff18089b5954ddf0ad844
Aakash <aakashamatya63@gmail.com>
2020-03-04 17:42:18 -0600

    implemented data provider

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,1 +1,2 @@
-export const API_ENDPOINT = 'https://localhost:44307/api';
+export const API_ENDPOINT = 'https://localhost:44358/api';
+export const GOOGLE_APP_ID = '18871307197-66akj9fkqpnhkoegsj2mfmj0mchcv02t.apps.googleusercontent.com';
--- src/utils/dataProvider.js
+++ src/utils/dataProvider.js
@@ -1,5 +1,48 @@
-import jsonServerProvider from 'ra-data-json-server';
+import simpleRestProvider from 'ra-data-simple-rest';
+import { fetchUtils } from 'react-admin';
+import { API_ENDPOINT } from '../constants/constants';
 
-const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
+let jwtToken = null;
+
+export const setAuthToken = (token) => {
+    jwtToken = token;
+};
+
+export const clearAuthToken = () => {
+    jwtToken = null;
+};
+
+const httpClient2 = async (url, options = {}) => {
+    let { headers, user } = options;
+
+    if (!headers) {
+        headers = new Headers({ Accept: 'application/json' });
+    }
+
+    if (!user && jwtToken) {
+        user = {
+            authenticated: true,
+            token: `Bearer ${jwtToken}`,
+        };
+    }
+
+    const result = await fetchUtils.fetchJson(url, {
+        ...options,
+        headers,
+        user,
+    });
+
+    const { status, headers: responseHeaders, json } = result;
+    if (status < 299 && json.code === 0) {
+        const { data } = json;
+        return {
+            json: data,
+            headers: responseHeaders,
+        }
+    }
+    throw new Error('something wrong');
+};
+
+const dataProvider = simpleRestProvider(API_ENDPOINT, httpClient2)
 
 export default dataProvider;
