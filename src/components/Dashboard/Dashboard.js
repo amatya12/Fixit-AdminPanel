e6652cd7b95172e26f4bb4d14fb660b3a87686fd
Aakash <aakashamatya63@gmail.com>
2020-03-04 17:44:44 -0600

    implemented Dashboard page

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -2,10 +2,13 @@ import React from 'react';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import { Title } from 'react-admin';
+import CardHeader from '@material-ui/core/CardHeader';
+
 
 export default () => (
     <Card>
-        <Title title="Welcome to FixIt Admin Panel." />
-        <CardContent>Test</CardContent>
+        <CardHeader title="Welcome to FixIt Admin Panel." />
+        {/* //<Title title="Welcome to Southeastern Admin Portal 2.0!" /> */}
+        <CardContent>Please select a menu item to begin...</CardContent>
     </Card>
 )
