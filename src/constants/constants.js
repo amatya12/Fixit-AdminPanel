c1348b6377f2075419c40ace9b9e7428e286c981
Aakash <aakashamatya63@gmail.com>
2020-03-22 19:33:09 -0500

    change the Google App client ID

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,4 +1,6 @@
 //export const API_ENDPOINT = 'https://localhost:44358/api';
 //export const API_ENDPOINT = 'http://192.168.1.249:5001/api';
-export const API_ENDPOINT = 'http://api.fixit-backend.com/';
-export const GOOGLE_APP_ID = '18871307197-66akj9fkqpnhkoegsj2mfmj0mchcv02t.apps.googleusercontent.com';
+export const API_ENDPOINT = 'http://api.fixit-backend.com/api';
+//export const GOOGLE_APP_ID = '18871307197-66akj9fkqpnhkoegsj2mfmj0mchcv02t.apps.googleusercontent.com';
+//export const GOOGLE_APP_ID = '183682296225-lljtll4ma1iaouelft6e744uduov9rlf.apps.googleusercontent.com';
+export const GOOGLE_APP_ID = '778840474528-3e3ts36rmp0du5q6bvvci09hcj3dn28u.apps.googleusercontent.com';
