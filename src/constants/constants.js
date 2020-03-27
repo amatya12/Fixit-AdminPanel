ea0e0f26d615d053d94e92d455e1bb675bdf6c01
Aakash <aakashamatya63@gmail.com>
2020-03-27 00:10:23 -0500

    asd

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -3,7 +3,7 @@
 export const API_ENDPOINT = 'http://api.fixit-backend.com/api';
 //export const GOOGLE_APP_ID = '18871307197-66akj9fkqpnhkoegsj2mfmj0mchcv02t.apps.googleusercontent.com';
 
-export const GOOGLE_APP_ID = '778840474528-krve94o3crspobee7o2eoto2fk49e2co.apps.googleusercontent.com';
-//export const GOOGLE_APP_ID = '778840474528-3e3ts36rmp0du5q6bvvci09hcj3dn28u.apps.googleusercontent.com';
+//export const GOOGLE_APP_ID = '778840474528-krve94o3crspobee7o2eoto2fk49e2co.apps.googleusercontent.com';
+export const GOOGLE_APP_ID = '778840474528-3e3ts36rmp0du5q6bvvci09hcj3dn28u.apps.googleusercontent.com';
 //export const GOOGLE_APP_ID = '183682296225-lljtll4ma1iaouelft6e744uduov9rlf.apps.googleusercontent.com';
 //export const GOOGLE_APP_ID = '778840474528-3e3ts36rmp0du5q6bvvci09hcj3dn28u.apps.googleusercontent.com';
