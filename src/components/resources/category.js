61e8f1e058bb93d5c1e3b26e33f49594579bf5f0
Aakash <aakashamatya63@gmail.com>
2020-04-08 01:34:25 -0500

    export function for category

--- src/components/resources/category.js
+++ src/components/resources/category.js
@@ -1,10 +1,38 @@
 import React from 'react';
 
-import { List, Datagrid, TextField, Filter, TextInput, Edit, SimpleForm, Create, ArrayField, SingleFieldList, ChipField }
+import { List, Datagrid, TextField, Filter, TextInput, Edit, SimpleForm, Create, ArrayField, SingleFieldList, ChipField, downloadCSV }
     from 'react-admin';
+import jsonExport from 'jsonexport/dist';
+
+import * as R from 'ramda';
+
+const exporter = (category) => {
+    console.log(category);
+    const data = category.map(record => ({
+        Id: record.id,
+        CategoryName: record.categoryName,
+        SubCategories: ParseSubCategory(record.subCategories),
+    }))
+    jsonExport(data, {
+    }, (err, csv) => {
+        downloadCSV(csv, 'category');
+    });
+};
+
+
+const ParseSubCategory = (subCategory) => {
+    const result = R.pipe(
+        R.map(R.prop('subCategoryName')),
+        R.join('|')
+
+    )(subCategory);
+
+    return result;
+}
+
 
 export const CategoryList = (props) => (
-    <List filters={<CategoryFilter />} {...props} >
+    <List filters={<CategoryFilter />} {...props} exporter={exporter}>
         <Datagrid rowClick="edit">
             <TextField source="id" />
             <TextField source="categoryName" />
